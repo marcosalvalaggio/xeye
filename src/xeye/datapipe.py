7590408ca9ff076c61a30ad29f434f6929f3ce29
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2022-08-03 21:48:08 +0200

    modified readme

--- src/xeye/datapipe.py
+++ src/xeye/datapipe.py
@@ -93,7 +93,7 @@ class dataset:
             
             font = cv2.FONT_HERSHEY_COMPLEX
             text = 'click on the image window and then press [q] on the keyboard to quit preview'
-            cv2.putText(frame,text,(0,50),font,0.8,(124,252,0),2)  #text,coordinate,font,size of text,color,thickness of font
+            cv2.putText(frame,text,(5,50),font,0.8,(124,252,0),2)  #text,coordinate,font,size of text,color,thickness of font
 
             cv2.imshow("Camera PreView", frame)
 
@@ -388,7 +388,7 @@ class dataset2:
             
             font = cv2.FONT_HERSHEY_COMPLEX
             text = 'click on the image window and then press [q] on the keyboard to quit preview'
-            cv2.putText(frame,text,(0,50),font,0.8,(124,252,0),2)  #text,coordinate,font,size of text,color,thickness of font
+            cv2.putText(frame,text,(5,50),font,0.8,(124,252,0),2)  #text,coordinate,font,size of text,color,thickness of font
 
             cv2.imshow("Camera PreView", frame)
 
