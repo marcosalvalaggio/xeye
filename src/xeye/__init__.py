ba55d7d76c0adc5177250d066583fc4597b0054b
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-01-28 15:19:10 +0100

    new version

--- src/xeye/__init__.py
+++ src/xeye/__init__.py
@@ -1,7 +1,7 @@
-from .dataset import Dataset
-from .fast_dataset import FastDataset
-from .manual_dataset import ManualDataset
-from .build_dataset import BuildDataset
+from .__dataset import Dataset
+from .__fast_dataset import FastDataset
+from .__manual_dataset import ManualDataset
+from .__build_dataset import BuildDataset
 
 #version
-__version__ = "0.35"
+__version__ = "0.36"
