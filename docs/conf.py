3244cf45b3fd607a4c02f71b08ed79b0041ca23c
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-04-13 23:04:37 +0200

    wip docs

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ copyright = '2023, Marco Salvalaggio'
 author = 'Marco Salvalaggio'
 
 # The full version, including alpha/beta/rc tags
-release = '0.38'
+release = '0.39'
 
 
 # -- General configuration ---------------------------------------------------
