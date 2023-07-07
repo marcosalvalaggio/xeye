1cbe37cb696e5fcd683abbd334a6ca54733c58bc
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-07-07 17:45:46 +0200

    wip

--- examples/xeye-example/fastdataset.py
+++ examples/xeye-example/fastdataset.py
@@ -11,10 +11,10 @@ standby_time = 0
 # percentage of images in the test set 
 perc = 0.2
 
-data = xeye.FastDataset(index = index, img_types = img_types, label = label,
-                        num = num, height = height, width = width, stand_by_time = standby_time)
+data = xeye.FastDataset(index=index, img_types=img_types, label=label,
+                        num=num, height=height, width=width, stand_by_time=standby_time)
 data.preview()
 data.rgb() # or data.gray()
-data.compress_train_test(perc = perc)
+data.compress_train_test(perc=perc)
 data.compress_all()
 data.just_compress("batch_test")
