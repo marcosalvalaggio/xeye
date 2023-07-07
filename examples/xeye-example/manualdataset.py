0fe50a8e2b5bafff3d1af578b57855257e18d146
mar.salvalaggio@gmail.com <55287795+Salva32@users.noreply.github.com>
2023-07-07 17:57:49 +0200

    wip

--- examples/xeye-example/manualdataset.py
+++ examples/xeye-example/manualdataset.py
@@ -7,12 +7,10 @@ label = ['keyboard', 'mouse']
 num = 20
 height = 100
 width = 100
-standby_time = 0
-# percentage of images in the test set 
 perc = 0.2
 
-data = xeye.ManualDataset(index = index, img_types = img_types, 
-                          label = label, num = num, height = height, width = width)
+data = xeye.ManualDataset(index=index, img_types=img_types, 
+                          label=label, num=num, height=height, width=width)
 data.preview()
 data.rgb()
 data.compress_train_test(perc = perc)
