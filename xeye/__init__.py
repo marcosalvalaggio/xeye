623353bf8261ac3f697c893c507fd2852859b8b7
marcosalvalaggio <mar.salvalaggio@gmail.com>
2024-01-23 23:01:33 +0100

    wip

--- xeye/__init__.py
+++ xeye/__init__.py
@@ -1,14 +1,2 @@
-from .dataset import Dataset
-from .fast_dataset import FastDataset
-from .manual_dataset import ManualDataset
-from .build_dataset import BuildDataset
-
 #version
-__version__ = "1.5"
-
-__all__ = [
-    "Dataset",
-    "FastDataset",
-    "ManualDataset",
-    "BuildDataset"
-]
+__version__ = "1.6"
---
+++ xeye/usb/__init__.py
@@ -0,0 +1,10 @@
+from .dataset import Dataset
+from .fast_dataset import FastDataset
+from .manual_dataset import ManualDataset
+
+__all__ = [
+    "Dataset",
+    "FastDataset",
+    "ManualDataset",
+    "BuildDataset"
+]
