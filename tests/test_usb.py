beb1a523e701b252b18ee42c39e6d5956b6f1e16
marcosalvalaggio <mar.salvalaggio@gmail.com>
2024-01-30 18:05:49 +0100

    wip

--- tests/test_usb.py
+++
@@ -1,7 +0,0 @@
-import xeye
-
-data = xeye.Dataset(index=0, img_types=2, label=['a', 'b'], num=10, height=100, width=100, stand_by_time=0)
-data.preview()
-data.gray()
-data.compress_train_test(perc=0.2)
-data.compress_all()
